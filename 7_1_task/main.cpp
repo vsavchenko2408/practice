22dc4833c17991ef63ae9ddca497012d47ce1eb6
Der Meister <v.savchenko2408@gmail.com>
2025-12-19 10:13:55 +0100

    7_1_task

---
+++ 7_1_task/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+#include <thread>
+
+int main()
+{
+    while(true)
+    {
+    system("clear");
+    auto now = std::chrono::system_clock::now();
+    std::time_t t = std::chrono::system_clock::to_time_t(now);
+    std::tm tm;
+#if defined(_WIN32)
+    localtime_s(&tm, &t);     // Windows (thread-safe)
+#else
+    localtime_r(&t, &tm);     // POSIX (thread-safe)
+#endif
+    std::cout << std::put_time(&tm, "%F %T") << '\n'; // "%F %T" -> YYYY-MM-DD HH:MM:SS
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    }
+    return 0;
+}
