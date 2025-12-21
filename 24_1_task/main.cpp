86cfca06fe41dde958ecc145e7730864e3205146
P15s <v.savchenko2408@gmail.com>
2025-12-21 12:49:11 +0100

    24_1_task

--- 24_1_task/main.cpp
+++ 24_1_task/main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 
-
-
+void show(int* arr, const int N)
+{
+    for(int i = 0; i < N; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}
 int main()
 {
-    int* arr = new int[10]{25, 12, 8, 15, 69, 32, 89, 66, 78, 6};
-
-
+    const int N = 10;
+    int* arr = new int[N]{25, 12, 8, 15, 69, 32, 89, 66, 78, 6};
+    int count = 0;
+    for(int i = 0; i < N; i++)
+    {
+        if(arr[i] %2 != 0)
+        {
+            arr[i] = 0;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    show(arr, N);
+    int* new_arr = new int[count];
+    for(int j = 0; j < N; j++)
+    {
+        int c = 0;
+        if(arr[j] != 0)
+        {
+            c++;
+        new_arr[c] = arr[j];
+        }
+    }
+show(new_arr, count);
     delete[] arr;
+    delete[] new_arr;
 }
